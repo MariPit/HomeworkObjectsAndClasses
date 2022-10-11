d8f123238cc2fbfdad2b02b03ff848582674fce3
Мария <mariya_piterskaya@ymail.com>
2022-10-12 01:08:49 +0300

    done

--- src/hw/Main.java
+++ src/hw/Main.java
@@ -1,7 +1,23 @@
 package hw;
 
 public class Main {
+
+
     public static void main(String[] args) {
-        //write your code here
+        Author Dostaevsky = new Author("Федор", "Доставский");
+        Book Idiot = new Book("Идиот", Dostaevsky, 1868);
+
+        Author Griboedov = new Author("Александр", "Грибоедов");
+        Book WoeFromWit = new Book("Горе от ума", Griboedov, 1833);
+
+        System.out.println("Название книги: " + Idiot.getTitle()
+                + " Автор: " + Dostaevsky.getName() + " " + Dostaevsky.getSurname()
+                + " Год издания:" + Idiot.getPublicationYear());
+        System.out.println("Название книги: " + WoeFromWit.getTitle()
+                + " Автор: " + WoeFromWit.getAuthor().getName() + " " + WoeFromWit.getAuthor().getSurname()
+                + " Год издания:" + WoeFromWit.getPublicationYear());
+
+        Idiot.setPublicationYear(1990);
+        System.out.println("Idiot = " + Idiot.getPublicationYear());
     }
-}
+    }
